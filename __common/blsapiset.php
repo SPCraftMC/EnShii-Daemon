67ebe121505c911e3929209a93493e31a44a3202
XiaMoHuaHuo_CN <mail@huahuo-cn.tk>
2023-06-15 22:23:36 +0800

    1

--- __common/blsapiset.php
+++ __common/blsapiset.php
@@ -2,7 +2,7 @@
 $BLSAPI = array(
     "root" => "https://mcskin.cn",
     "grant_type" =>	"authorization_code",
-    "client_id" => 67,
-    "client_secret" => "hC7DDYZ7GH5pGGaN6rIpoA4ETEofI0HrZeijlgu1",
-    "redirect_uri" => "http://localhost:5173/auth/login",
+    "client_id" => <client_id>,
+    "client_secret" => "<client_secret>",
+    "redirect_uri" => "https://<panel_url>/auth/login",
 );
