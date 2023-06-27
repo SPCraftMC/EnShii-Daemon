bcccd8d923bd001350e02a727d0467e64b577c65
XiaMoHuaHuo_CN <mail@huahuo-cn.tk>
2023-06-27 23:35:17 +0800

    use nodejs

--- __common/blsapiset.php
+++
@@ -1,8 +0,0 @@
-<?php
-$BLSAPI = array(
-    "root" => "https://mcskin.cn",
-    "grant_type" =>	"authorization_code",
-    "client_id" => 67,
-    "client_secret" => "hC7DDYZ7GH5pGGaN6rIpoA4ETEofI0HrZeijlgu1",
-    "redirect_uri" => "http://localhost:5173/auth/login",
-);
