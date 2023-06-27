62ce5fd309b359c9b4eaaa1bb0b925202e455a52
XiaMoHuaHuo_CN <mail@huahuo-cn.tk>
2023-06-28 00:59:03 +0800

    fastlogin service info

--- modules/server/yggdrasil_server.js
+++ modules/server/yggdrasil_server.js
@@ -1,3 +1,40 @@
+const yconfig = require('../../util/config').fastloginConfig;
+
 module.exports = (req, res) => {
-    res.send("OK")
+    let resp = {}
+    if (yconfig.enable_fast_login) resp = {
+        status: true,
+        message: "",
+        data: {
+            fastlogin: yconfig.enable_fast_login,
+            services: getServices()
+        }
+    }
+    else resp = {
+        status: true,
+        message: "",
+        data: {
+            fastlogin: yconfig.enable_fast_login
+        }
+    }
+    res.setHeader("content-type", "application/json")
+    res.status(200)
+    res.send(resp)
+}
+
+function getServices() {
+    let services = []
+    yconfig.services.forEach(it => {
+        let ts = {
+            name: it.name,
+            id: it.id,
+            oauth: {
+                api_uri: it.api_root,
+                client_id: it.oauth_app.client_id,
+                client_secret: it.oauth_app.client_secret
+            }
+        }
+        services.push(ts)
+    });
+    return services
 }
