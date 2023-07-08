e9cc8634c76e4b800ae17a4126076995a3b042d2
XiaMoHuaHuo_CN <mail@huahuo-cn.tk>
2023-07-08 16:49:59 +0800

    dotenv template

--- database/init.js
+++ database/init.js
@@ -8,7 +8,7 @@ module.exports = () => {
   return new Promise((resolve, reject) => {
     exec('npx prisma db push', (error, stdout, stderr) => {
       if (error) {
-        logger.error(`error: ${error}`);
+        logger.error(`${error}`);
         logger.error("[init]Error while initializing the databasemodel!");
         logger.error("EnShii-Daemon will exit now.");
         process.exit();
@@ -17,13 +17,13 @@ module.exports = () => {
       logger.info("Database model initialization successful.");
       exec('npx prisma db seed', (error, stdout, stderr) => {
         if (error) {
-          logger.error(`error: ${error}`);
+          logger.error(`${error}`);
           logger.error("[init]Error while prepare root user!");
           logger.error("EnShii-Daemon will exit now.");
           process.exit();
         }
         logger.info(`stdout: ${stdout}`);
-        logger.info("The root user fit the requirements. initialization successful");
+        logger.info("The root user fit the requirements. Initialization successful.");
         resolve()
       });
     });
