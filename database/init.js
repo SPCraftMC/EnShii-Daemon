3d4fa2659432dec10a820cef2f441aa604ed1660
shiftz300 <kasetry300@outlook.com>
2023-07-18 22:01:54 +0800

    fix db init

--- database/init.js
+++ database/init.js
@@ -6,35 +6,25 @@ const { exec } = require('child_process');
 //执行shell命令，根据'../prisma/schema.prisma'初始化数据库
 module.exports = () => {
   return new Promise((resolve, reject) => {
-    exec('npx prisma generate', (error, stdout) => {
+    exec('npx prisma db push', (error, stdout) => {
       if (error) {
         logger.error(`${error}`);
-        logger.error("[init]Error while initializing the database client!");
+        logger.error("[init]Error while initializing the database model!");
         logger.error("EnShii-Daemon will exit now.");
         process.exit();
       }
-      logger.info(`${stdout}`)
-      logger.info("Database client initialization successful.")
-      exec('npx prisma db push', (error, stdout) => {
+      logger.info(`${stdout}`);
+      logger.info("Database model initialization successful.");
+      exec('npx prisma db seed', (error, stdout) => {
         if (error) {
           logger.error(`${error}`);
-          logger.error("[init]Error while initializing the database model!");
+          logger.error("[init]Error while preparing root user!");
           logger.error("EnShii-Daemon will exit now.");
           process.exit();
         }
         logger.info(`${stdout}`);
-        logger.info("Database model initialization successful.");
-        exec('npx prisma db seed', (error, stdout) => {
-          if (error) {
-            logger.error(`${error}`);
-            logger.error("[init]Error while preparing root user!");
-            logger.error("EnShii-Daemon will exit now.");
-            process.exit();
-          }
-          logger.info(`${stdout}`);
-          logger.info("The root user fit the requirements. Initialization successful.");
-          resolve()
-        });
+        logger.info("The root user fit the requirements. Initialization successful.");
+        resolve()
       });
     });
   });
