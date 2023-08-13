56f60a40e0a41c1b12e53d70fea7b6d5e5fbfa1b
XiaMoHuaHuo_CN <xmcn@1l1.icu>
2023-08-14 00:36:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
 //前部json内容；后部部分值写入环境变量供prisma生成客户端
+const config = {
+  host: "127.0.0.1",
+  port: 15450,
+  database {
+    method: "mysql",
+    mysql: {
+      host: "127.0.0.1",
+      port: 3306,
+      username: "enshii",
+      password: "enshii",
+      database: "enshii"
+    }
+  }
+}
