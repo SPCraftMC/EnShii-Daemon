0e2087f33ed2d647a5ad58d6e2af72356b432883
shiftz <130174875+shiftz300@users.noreply.github.com>
2023-08-15 19:25:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 //前部json内容；后部部分值写入环境变量供prisma生成客户端
 const config = {
-  host:127.0.0.1,
+  host: "127.0.0.1",
   port: 15450,
   database: {
     // method: {...}
     provider: "mysql",
-    host: 127.0.0.1,
+    host: "127.0.0.1",
     port: 3306,
     user: "enshii",
     password: "enshii",
