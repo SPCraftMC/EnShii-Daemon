4d2e0ada7d7c33c47efe4dcf8eb8e309dddb4556
shiftz <130174875+shiftz300@users.noreply.github.com>
2023-08-15 12:45:54 +0800

    Update infomation.js

--- modules/user/infomation.js
+++ modules/user/infomation.js
@@ -1,5 +1,5 @@
-const { verify } = require("../token");
-const { user } = require("../../database/db");
+const { verify } = require("../../token");
+const { user } = require("../database/db");
 
 module.exports = (req, res) => {
     const resp = {
