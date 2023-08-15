558a372bfb64440ff99198a352b19f69fe17219d
shiftz <130174875+shiftz300@users.noreply.github.com>
2023-08-15 12:41:41 +0800

    Update db.js

--- modules/database/db.js
+++ modules/database/db.js
@@ -1,19 +1,10 @@
 //集线器文件
 const init = require('./init')
 const user = require("./user")
-const token = require("../modules/token")
 const blacklist = require("./blacklist")
 
 module.exports = {
     init: init,
     user: user,
-    token: token,
     blacklist: blacklist
 }
-/*
-权限
--1 - banned
-1 - user
-2 - admin
-3 - root
-*/
