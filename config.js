4912504c9e0902fdc9f1448001ced888573bba9c
shiftz <130174875+shiftz300@users.noreply.github.com>
2023-08-15 13:25:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const config = {
     mysql: {
       host: "127.0.0.1",
       port: 3306,
-      username: "enshii",
+      user: "enshii",
       password: "enshii",
       database: "enshii"
     },
